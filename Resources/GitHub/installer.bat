9ffdd5688af21114178a0cc98c86861159fedea1
everest. <isaaclevirandle@gmail.com>
2026-08-17 09:22:33 +0100

    Fix C drive install bug, and give more info while installing.

--- Resources/GitHub/installer.bat
+++ Resources/GitHub/installer.bat
@@ -7,65 +7,73 @@ chcp ANSI
 
 cls
 title Seralyth Menu Installer // [#---------] Getting directory
+echo Seralyth Menu Installer // [#---------] Getting directory
 color 0e
 
 :: Thanks to tdcvoid for telling me the new path for Oculus
-set steamPath1="C:/Program Files (x86)/Steam/steamapps/common/Gorilla Tag"
-set steamPath2="C:/Program Files/Meta Horizon/Software/Software/another-axiom-gorilla-tag" 
-
-if exist %steamPath1% (
-    set gamePath=%steamPath1%
-) else if exist %steamPath2% (
-    set gamePath=%steamPath2%
-) else if exist %steamPath3% (
-    set gamePath=%steamPath3%
-) else if exist %steamPath4% (
-    set gamePath=%steamPath4%
-) else (
-    color 0c
-    set /p userPath=Gorilla Tag directory not found.
-    pause
-    exit /b
+if exist "C:/Program Files (x86)/Steam/steamapps/common/Gorilla Tag" (
+    set "gamePath=C:/Program Files (x86)/Steam/steamapps/common/Gorilla Tag"
+     goto afterDriveSearch
+)
+else if exist "C:/Program Files/Meta Horizon/Software/Software/another-axiom-gorilla-tag" (
+    set "gamePath=C:/Program Files/Meta Horizon/Software/Software/another-axiom-gorilla-tag"
+     goto afterDriveSearch
 )
 
+:: Deep search made by Lucky & fixed by everest
 if not defined gamePath (
-    for %%D in (E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+     for %%D in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
         if exist "%%D:/SteamLibrary/steamapps/common/Gorilla Tag" (
             set "gamePath=%%D:/SteamLibrary/steamapps/common/Gorilla Tag"
             goto afterDriveSearch
         )
-        if exist "%%D:/Steam/steamapps/common/Gorilla Tag" (
+        else if exist "%%D:/Steam/steamapps/common/Gorilla Tag" (
             set "gamePath=%%D:/Steam/steamapps/common/Gorilla Tag"
             goto afterDriveSearch
         )
     )
 )
 
+:afterDriveSearch
+
+if not defined gamePath (
+    color 0c
+    echo Gorilla Tag directory not found.
+    echo Please open a mod-help ticket in the Discord server and report this. (https://discord.gg/seralyth)
+    pause
+    exit /b
+)
+
 color 0e
 cls
 title Seralyth Menu Installer // [###-------] Downloading BepInEx
-curl -L "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.4/BepInEx_win_x64_5.4.23.4.zip" -o BPNX54234.zip
+echo Seralyth Menu Installer // [###-------] Downloading BepInEx
+curl -L "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.4/BepInEx_win_x64_5.4.23.4.zip" -o .tmp-BPNX54234.zip
 
 powershell -command "Expand-Archive -Path 'BPNX54234.zip' -DestinationPath '%gamePath%' -Force"
 
 cls
 title Seralyth Menu Installer // [####------] Creating directories
+echo Seralyth Menu Installer // [####------] Creating directories
 mkdir %gamePath%/BepInEx/config
 mkdir %gamePath%/BepInEx/plugins
 
 cls
 title Seralyth Menu Installer // [#####-----] Downloading latest config
+echo Seralyth Menu Installer // [#####-----] Downloading latest config
 curl https://github.com/Seralyth/Seralyth-Menu/raw/refs/heads/master/Resources/GitHub/BepInEx.cfg -o %gamePath%/BepInEx/config/BepInEx.cfg
 
 cls
 title Seralyth Menu Installer // [#######---] Downloading menu
+echo Seralyth Menu Installer // [#######---] Downloading menu
 for /f "tokens=*" %%i in ('powershell -Command "(Invoke-RestMethod -Uri 'https://api.github.com/repos/Seralyth/Seralyth-Menu/releases/latest').assets | Where-Object { $_.name -like '*.dll' } | Select-Object -ExpandProperty browser_download_url"') do (
     set pluginUrl=%%i
 )
 
 if "%pluginUrl%"=="" (
     color 0c
-    echo Failed to get latest release of menu, please report to Discord
+    echo Failed to get latest release of menu.
+    echo Please open a mod-help ticket in the Discord server amd report this. (https://discord.gg/seralyth)
     pause
     exit /b
 )
@@ -75,8 +83,9 @@ curl -L "%pluginUrl%" -o %gamePath%/BepInEx/plugins/"Seralyth Menu.dll"
 
 cls
 title Seralyth Menu Installer // [##########] Finished
+echo Seralyth Menu Installer // [##########] Finished
 echo Congratulations, you now have the menu!
 
-del "BPNX54234.zip"
+del ".tmp-BPNX54234.zip"
 
 pause
